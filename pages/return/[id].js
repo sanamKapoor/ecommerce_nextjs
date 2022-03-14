f31606452b7e9574840691e6a78435b0ceb37b4b
sanamKapoor <sanamkapoor3285@gmail.com>
2022-03-14 23:03:09 +0530

    desktop design done

--- pages/return/[id].js
+++ pages/return/[id].js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Header from "../../components/Header.js/index.js";
 import Footer from "../../components/Footer";
 import Heading from "../../components/Global/Heading";
-import ReturnDetail from "../../components/Return/index";
+import ReturnDetail from "../../components/Return";
 
 const Index = () => {
     return(
